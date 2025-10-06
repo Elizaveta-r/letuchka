22e0435b1276f5aa6f13cb6dab0560539569d208
Elizaveta Zhigunova <223254661+Elizaveta-r@users.noreply.github.com>
2025-10-06 13:14:06 +0300

    Update BaseLayout.jsx

--- src/layout/BaseLayout/BaseLayout.jsx
+++ src/layout/BaseLayout/BaseLayout.jsx
@@ -4,11 +4,11 @@ import { selectIsLoggedIn } from "../../store/slices/userSlice";
 import styles from "./BaseLayout.module.scss";
 
 export const BaseLayout = ({ children }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+  // const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  if (!isLoggedIn) {
-    return <Navigate to="/auth" replace />;
-  }
+  // if (!isLoggedIn) {
+  //   return <Navigate to="/auth" replace />;
+  // }
 
   return <div className={styles.layout}>{children}</div>;
 };
